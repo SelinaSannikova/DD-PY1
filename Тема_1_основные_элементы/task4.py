4ee36a326849f09e6e3c0b93a7fc3175bfb39601
SelinaSannikova <115585215+SelinaSannikova@users.noreply.github.com>
2022-10-19 01:57:28 +0300

    Add files via upload

---
+++ Тема_1_основные_элементы/task4.py
@@ -0,0 +1,5 @@
+begin_with_char = "begin_with_char"  # название переменной не должно начинаться с цифры
+one_word_for_var = "one_word_for_var"  # название переменной должно состоять из одного слова
+
+print(begin_with_char)# TODO распечатать содержимое первой переменной
+print(one_word_for_var)# TODO распечатать содержимое второй переменной
